dfef6e27e635b90848d539a3e530bfd4a8591815
+ <+>
2024-07-24 23:32:22 +0300

    completed test

---
+++ index.js
@@ -0,0 +1,19 @@
+function receivesAFunction(callback) {
+    return callback();
+  }
+  
+  
+  function add(a, b) {
+    return a + b;
+  }
+  
+  function returnsANamedFunction() {
+    return add;
+  }
+ 
+  
+  function returnsAnAnonymousFunction() {
+      return function () {
+          console.log("Anonymous!")
+      }
+  }
